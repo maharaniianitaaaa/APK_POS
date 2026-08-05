e5152c317a680b7a8c49fe8859dcc645cb809d02
maharaniianitaaaa <maharaniianitaaaa@gmail.com>
2026-08-05 09:36:08 +0700

    Add files via upload

---
+++ database/factories/ItemPenjualanFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ItemPenjualan;
+use App\Models\Produk;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemPenjualanFactory extends Factory
+{
+    protected $model = ItemPenjualan::class;
+
+    public function definition(): array
+    {
+        $produk = Produk::inRandomOrder()->first();
+        $qty = $this->faker->numberBetween(1, 5);
+
+        return [
+            'produk_id' => $produk->id,
+            'kuantitas' => $qty,
+            'harga_satuan' => $produk->harga_jual,
+            'subtotal' => $produk->harga_jual * $qty,
+        ];
+    }
+}
---
+++ database/factories/PenjualanFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Penjualan;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PenjualanFactory extends Factory
+{
+    protected $model = Penjualan::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->value('id'),
+            'total_pembayaran' => 0,
+            'metode_pembayaran' => $this->faker->randomElement([
+                'CASH', 'TRANSFER', 'QRIS'
+            ]),
+            'status' => $this->faker->randomElement([
+                'OPEN', 'COMPLETED'
+            ]),
+        ];
+    }
+}
